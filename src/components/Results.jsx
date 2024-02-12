a606847863c1fcecc45bf9f3f115612bc45efafd
Kristjan Lääne <xonproject@gmail.com>
2024-02-12 11:44:58 +0200

    Number bug

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,9 @@
+import { calculateInvestmentResults } from '../util/investment.js';
+
 export const Results = ({ input }) => {
-  console.log(input);
+  const resultsData = calculateInvestmentResults(input);
+
+  console.log(resultsData);
+
   return <p>Results...</p>;
 };
