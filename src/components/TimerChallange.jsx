2b467c611c039f340c9bbc918c35154e21295984
Kristjan Lääne <xonproject@gmail.com>
2024-02-20 18:54:39 +0200

    TimerChallenge

---
+++ src/components/TimerChallange.jsx
@@ -0,0 +1,14 @@
+export const TimerChallange = ({ title, targetTime }) => {
+  return (
+    <section className='challenge'>
+      <h2>{title}</h2>
+      <p className='challenge-time'>
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+      <p>
+        <button>Start challange</button>
+      </p>
+      <p className=''>Time is running...</p>
+    </section>
+  );
+};
