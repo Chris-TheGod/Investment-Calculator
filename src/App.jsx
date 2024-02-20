982d23861f988bc8ed2e85f4424c2b68dab8f07e
Kristjan Lääne <xonproject@gmail.com>
2024-02-20 18:14:23 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
-import { useState } from 'react';
-import { Header } from './components/Header';
-import { UserInput } from './components/UserInput';
-import { Results } from './components/Results';
+import Player from './components/Player.jsx';
 
 function App() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  const inputIsValid = userInput.duration >= 1;
-
-  const handleChange = (inputIdentifier, newValue) => {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: +newValue,
-      };
-    });
-  };
-
   return (
     <>
-      <Header />
-      <UserInput onChange={handleChange} userInput={userInput} />
-      {!inputIsValid && (
-        <p className='center'>Please enter a duration greater than 0</p>
-      )}
-      {inputIsValid && <Results input={userInput} />}
+      <Player />
+      <div id="challenges"></div>
     </>
   );
 }
